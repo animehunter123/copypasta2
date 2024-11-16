dad5a24b1769f2f0673c2ad52724a48f7328da34
kenshin <kenshin@barret.local>
2024-11-16 13:54:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+* Description
+
+Just a webapp to emulate the copypasta, but in vanilla js.
+
+* Todo
+
+make gitignore
+make bash script for node ./server.js
---
+++ start-webapp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Ensure that nodejs is installed or exit with a message to the user
+command -v node >/dev/null 2>&1 || { echo >&2 "Node.js is required but it's not installed.  Aborting."; exit 1; }
+
+# Ensure that npm is installed or exit with a message to the user
+command -v npm >/dev/null 2>&1 || { echo >&2 "npm is required but it's not installed.  Aborting."; exit 1; }
+
+# Ensure that node_modules is installed, if not installs and creates the directory
+if [ ! -d "./node_modules" ]; then
+    echo "Installing node_modules"
+    npm install
+fi
+
+# Create the ./data directory if it doesn't exist
+mkdir -p ./data 
+
+# Inform the user which port the web application is running on
+echo "Web application will soon run on port 3000"
+
+# Start the web application
+node server.js
