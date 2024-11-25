1f0b00728f0f81e00d8a81b4323c6dda70048c12
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2024-11-25 11:16:28 +0900

    added a note for podman

--- docker-build.(DO NOT USE YET)/Dockerfile_Build.sh
+++ docker-build.(DO NOT USE YET)/Dockerfile_Build.sh
@@ -8,6 +8,6 @@ docker rm -f $target 2>/dev/null
 docker image rm -f $target 2>/dev/null
 
 echo "Building the new container: $target from the internet..."
-docker build -t $target .
+docker build -t $target .  #You can also use "podman in place of docker here..."
 
 echo "Script complete. Confirm it with 'docker images' to confirm container $target:latest is now listed"
