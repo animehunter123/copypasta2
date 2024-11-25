96cad0d229f6b51b34550b67d41da1761db61636
Your Name <you@example.com>
2024-11-25 22:25:59 +0900

    fixed install-deps host script, to ensure Ubuntu 2404 Hosts have MeteorJS 3.1 as a baseline moving forward

--- install-deps-on-host.sh
+++ install-deps-on-host.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Install NodeJS 
+sudo apt update
+sudo apt install -y nodejs npm
+
+# Install MeteorJS (3.1)
+curl https://install.meteor.com/\?release\=3.1 | sh
+
 # Its a good idea to ensure that the .node_modules and .meteor are updated, so if you need to fetch the dependencies, uncomment the below...
 cd webapp-meteor
 npm install        # This populates the ./node_modules directory
