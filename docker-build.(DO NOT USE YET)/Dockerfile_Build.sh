738be74d758738bcf62e4c2877d30e32a75ea5e8
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2024-11-25 14:42:04 +0900

    Added prepartion to upgrade from Meteor 3.0.4 to 3.1.0

--- docker-build.(DO NOT USE YET)/Dockerfile_Build.sh
+++ docker-build.(DO NOT USE YET)/Dockerfile_Build.sh
@@ -9,6 +9,7 @@ docker image rm -f $target 2>/dev/null
 
 echo "Building the new container: $target from the internet..."
 # Or on Windows/WSL/Podman set network=host on wsl...: podman build -t docker-meteorjs-webapp . --network=host
+# Note on podman windows, you can manually run it without docker-compose via: podman run  -it --rm -p 3000:3000 -v "%cd%\\..\\..:/app" docker-meteorjs-webapp bash
 docker build -t $target .  
 
 echo "Script complete. Confirm it with 'docker images' to confirm container $target:latest is now listed"
