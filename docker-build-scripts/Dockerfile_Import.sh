a1c5d45fa362aa30ed9443a982498a29364111c1
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2024-11-27 18:57:00 +0900

    fixed the import by ensuring the image already exists

--- docker-build-scripts/Dockerfile_Import.sh
+++ docker-build-scripts/Dockerfile_Import.sh
@@ -3,8 +3,10 @@
 # Standard name for our docker container
 target="docker-meteorjs-webapp"
 
-echo "Importing image to $target:latest..."
+echo "Removing $target if it exists..."
+docker image rm -f $target:latest
 
+echo "Importing image to $target:latest..."
 # If its a bz2 load it, else if its a tar load it
 if [ -f ./$target.tar.bz2 ]; then
     echo "Loading $target.tar.bz2"
