da94e957f33653780e4c64a48f3377728b2bde12
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2024-11-27 19:00:01 +0900

    added legacy docker commands just in case users opt for it

--- docker-build-scripts/logs-webapp-on-docker.sh
+++ docker-build-scripts/logs-webapp-on-docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 docker compose logs -f
+docker-compose logs -f
--- docker-build-scripts/restart-webapp-on-docker.sh
+++ docker-build-scripts/restart-webapp-on-docker.sh
@@ -2,6 +2,7 @@
 
 echo "Stopping the docker-meteorjs-webapp container..."
 docker compose down
+docker-compose down
 
 echo "Starting the docker-meteorjs-webapp container..."
-docker compose up -d
+docker-compose up -d
